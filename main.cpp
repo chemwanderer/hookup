883fe4e93c77bc9e92db75a1df8e301e2e17b94f
chemwanderer <142178085+chemwanderer@users.noreply.github.com>
2024-10-20 15:08:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include <QtWidgets/QApplication>
+
+#include "engine/engine_terminal.h"
+#include "gui/gui_terminal.h"
+
+int main(int argc, char *argv[])
+{
+    // configurate the engine
+    if (!engine::Terminal::build()) { return -1; }
+
+    QApplication a(argc, argv);
+
+    // create & configurate GUI
+    gui::Terminal::build();
+    QObject::connect(&a, &QApplication::aboutToQuit, gui::Terminal::terminal(), &gui::Terminal::deleteLater);
+
+    // launch the data exchange & GUI update loop
+    engine::Terminal::launch();
+    gui::Terminal::launch();
+
+    int appRes = a.exec();
+
+    // stop the engine
+    engine::Terminal::halt();
+
+    return appRes;
+}
