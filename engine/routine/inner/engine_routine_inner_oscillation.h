ba59b7fcc5825962cb9c1f9c94ff950bfad33b98
chemwanderer <142178085+chemwanderer@users.noreply.github.com>
2024-10-21 16:18:29 +0300

    Update engine_routine_inner_oscillation.h

--- engine/routine/inner/engine_routine_inner_oscillation.h
+++ engine/routine/inner/engine_routine_inner_oscillation.h
@@ -15,7 +15,7 @@
 // b is the offset;					    //
 // pi is the number π.					    //
 //							    //
-// Can be used for changing voltage level sended to DAC in  //
+// Can be used for changing voltage level sent to DAC in    //
 // time, for example.					    //
 //							    //
 //////////////////////////////////////////////////////////////
